e1ecec8b1fb9ff4d09e118ce4e0e3ab9a59c6f80
Replitmygoat <4986@trinity.croydon.sch.uk>
2025-04-25 13:06:09 +0100

    Update level1_tasks.py

--- level1_tasks.py
+++ level1_tasks.py
@@ -1,1 +1,7 @@
-print("Hello")
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(1)
+name= input("hello please enter your name")
+   
